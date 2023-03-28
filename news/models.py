9bc536c41ac2111336d4286ae7b6559f393d4553
adetunji peter <peteradetunji30@gmail.com>
2023-03-28 21:22:25 +0100

    initial commit

--- news/models.py
+++ news/models.py
@@ -1,5 +1,10 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
+class News(models.Model):
+    text=models.TextField()
+
+
+    def __str__(self):
+        return self.text[:50]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import News
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model=News
     template_name='home.html'
+    context_object_name='all_post_list'
 
-class AboutPageView(TemplateView):
-    template_name='about.html'
